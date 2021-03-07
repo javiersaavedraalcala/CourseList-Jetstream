76ecd8aaa35447d9315ce05a2f92b1e70ab9b367
Javier Saavedra <javier_saavedra_alcala@outlook.es>
2021-03-07 00:53:15 -0600

    First commit

---
+++ resources/views/layouts/web.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Cursos de Programación Web</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+    <body>
+        <header class="shadow-lg">
+            <div class="bg-blue-600 py-1">
+                <nav class="bg-blue-500 py-2">
+                    <a href="{{ route('home') }}">
+                        <img src="{{ asset('images/courses.png') }}" class="h-8 mx-auto">
+                    </a>
+                </nav>
+            </div>
+        </header>
+
+        <main class="py-10">
+            <div class="container mx-auto px-4">
+                @yield('content')
+            </div>
+        </main>
+
+        <footer class="py-4 text-center">
+            @auth
+                <a href="{{ url('dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
+            @else
+            <a href="{{ url('login') }}" class="text-sm text-gray-700 underline">Login</a>
+            <a href="{{ url('register') }}" class="ml-4 text-sm text-gray-700 underline">Registro</a>
+            @endif
+        </footer>
+    </body>
+</html>
